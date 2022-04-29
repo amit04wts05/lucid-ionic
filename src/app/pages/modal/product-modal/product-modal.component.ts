415e1d89fbc5a58885709750c96cf3ee86d7cf87
amit kumar <amit04wts05@gmail.com>
2022-04-29 20:50:20 +0530

    changes

--- src/app/pages/modal/product-modal/product-modal.component.ts
+++ src/app/pages/modal/product-modal/product-modal.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {  ModalController, NavParams } from '@ionic/angular';
+import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
+import {  IonSlides, ModalController, NavParams } from '@ionic/angular';
 
 @Component({
   selector: 'app-product-modal',
@@ -7,15 +7,32 @@ import {  ModalController, NavParams } from '@ionic/angular';
   styleUrls: ['./product-modal.component.scss'],
 })
 export class ProductModalComponent implements OnInit {
+qty:any = 1;
+size:any ;
+@ViewChild('sliderIndex', { static: false }) slides: IonSlides;
+
 
   constructor(private modalController: ModalController,
     private navParams: NavParams) { }
-    
+
     async closeModal() {
       const onClosedData: string = "Wrapped Up!";
       await this.modalController.dismiss(onClosedData);
     }
+qtyIncrease(){
 
+  this.qty+=this.qty;
+}
+qtyDecriment(){
+  if(this.qty>0){
+    this.qty-=this.qty;
+  }
+
+}
+selectedImage(i){
+  this.slides.slideTo(i);
+  // this.cd.detectChanges();
+}
   ngOnInit() {}
 
 }
--- src/app/pages/pos/pos.module.ts
+++ src/app/pages/pos/pos.module.ts
@@ -5,10 +5,11 @@ import { PosRoutingModule } from "./pos-routing.module";
 import { PosComponent } from "./pos.component";
 import { ProductModalComponent } from "../modal/product-modal/product-modal.component";
 import { MainLayoutModule } from "src/app/layouts/main/main.module";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [PosComponent,ProductModalComponent],
-  imports: [CommonModule, MainLayoutModule, PosRoutingModule, SharedModule],
+  imports: [CommonModule, MainLayoutModule, PosRoutingModule, SharedModule,FormsModule],
   exports: [SharedModule],
 })
 export class PosModule {}
