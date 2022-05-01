b497553b36e30adf3cf83e77cd4f1b0ccb8876a7
Amit kumar <amit.chauhan@copancs.com>
2022-05-01 17:18:11 +0530

    DESIGN DONE

--- src/app/pages/modal/product-modal/product-modal.component.ts
+++ src/app/pages/modal/product-modal/product-modal.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
-import {  IonSlides, ModalController, NavParams } from '@ionic/angular';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { IonSlides, ModalController, NavParams } from '@ionic/angular';
 
 @Component({
   selector: 'app-product-modal',
@@ -7,32 +7,49 @@ import {  IonSlides, ModalController, NavParams } from '@ionic/angular';
   styleUrls: ['./product-modal.component.scss'],
 })
 export class ProductModalComponent implements OnInit {
-qty:any = 1;
-size:any ;
-@ViewChild('sliderIndex', { static: false }) slides: IonSlides;
-
-
-  constructor(private modalController: ModalController,
-    private navParams: NavParams) { }
-
-    async closeModal() {
-      const onClosedData: string = "Wrapped Up!";
-      await this.modalController.dismiss(onClosedData);
+  qty: any = 1;
+  size: any;
+  @ViewChild('sliderIndex',  {static: false}) viewer: IonSlides;
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    loop: true,
+    autoplay: {
+          delay: 2000
+    },
+    pagination : {
+      el: '.swiper-pagination',
+      clickable: true,
     }
-qtyIncrease(){
 
-  this.qty=this.qty+1;
-}
-qtyDecriment(){
-  if(this.qty>0){
-    this.qty=this.qty-1;
   }
+  constructor(
+    private modalController: ModalController,
+    private navParams: NavParams
+  ) {}
 
+  async closeModal() {
+    const onClosedData: string = 'Wrapped Up!';
+    await this.modalController.dismiss(onClosedData);
+  }
+  qtyIncrease() {
+    this.qty = this.qty + 1;
+  }
+  qtyDecriment() {
+    if (this.qty > 0) {
+      this.qty = this.qty - 1;
+    }
+  }
+  onSlideMoved(event) {
+   // this.slides.slideTo(i);
+   event.target.isEnd().then(isEnd => {
+    console.log('End of slide', isEnd);
+  });
+
+  event.target.isBeginning().then((istrue) => {
+    console.log('End of slide', istrue);
+  });
 }
-selectedImage(i){
-  this.slides.slideTo(i);
-  // this.cd.detectChanges();
-}
+    // this.cd.detectChanges();
   ngOnInit() {}
-
 }
