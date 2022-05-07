496f515fe74bd4a19de3b0beaf2e113e234f7cfa
Amit kumar <amit.chauhan@copancs.com>
2022-05-07 21:06:03 +0530

    commit

---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { AuthService } from './auth-service';
+
+@Injectable()
+export class AuthGuardService implements CanActivate {
+  constructor(private router: Router, private authService: AuthService,private navCtr:NavController) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | UrlTree {
+   let authtoken= JSON.parse(localStorage.getItem("_user"));
+   if (!authtoken.token) { 
+     alert("xvcxv")
+      //this.router.navigate([''], { queryParams: { retUrl: route.url } });
+     // this.navCtr.navigateRoot([''])
+      return false;
+
+      //var urlTree = this.router.createUrlTree(['login']);
+      //return urlTree;
+    }
+
+    return true;
+  }
+}
